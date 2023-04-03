f7f8080074de33c844e73c539a587b90af9e8889
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-04-03 20:56:37 +0300

    kesken 3.4.

--- js_moduuli1/t9/01teht9.js
+++ js_moduuli1/t9/01teht9.js
@@ -1,9 +1,41 @@
 const integer = prompt('Give me an integer and I will tell you if it is a prime number:');
 
+let multiplication;
+    for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= 5; j++) {
+           multiplication = i * j;
+            console.log(i + ' times ' + j + ' is ' + multiplication + ".");
+        }
+    }
 
-luku = int(luku)
-        total = 0
 
+let total = 2;
+if (integer == 1 || 2){
+  document.querySelector('#target').innerHTML='The given number, ' + integer + ' is a prime number.';
+}
+else{
+  while (total < integer){
+    if (integer % total == 0){
+      document.querySelector('#target').innerHTML='The given number, ' + integer + ' is not a prime number.';
+      console.log(total)
+      return (total);
+
+    }
+    else {
+        total = total + 1;
+        console.log(total)
+  }
+  }}
+console.log(total)
+if (total == integer) {
+  document.querySelector('#target').innerHTML='The given number, ' + integer + ' is a prime number.';
+    }
+
+
+
+
+
+/*
         for n in range(1, luku):
             if luku % n == 0:
                 total = total + 1
@@ -19,5 +51,5 @@ luku = int(luku)
                 "isPrime": False
             }
 
-document.querySelector('#target').innerHTML='The given number, ' + integer + 'is a prime number.'
-document.querySelector('#target').innerHTML='The given number, ' + integer + 'is not a prime number.'
+document.querySelector('#target').innerHTML='The given number, ' + integer + 'is a prime number.';
+document.querySelector('#target').innerHTML='The given number, ' + integer + 'is not a prime number.';*/
