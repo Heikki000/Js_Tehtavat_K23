12d1d56a667c497384135105dd76cfbafa258c5c
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-04-10 13:43:13 +0300

    valmis 10.4.

---
+++ js_moduuli2/t3/02teht3.js
@@ -0,0 +1,21 @@
+alert(`Give me 6 dogs' names: `)
+
+let dogs = []
+
+for (i = 0; i < 6; i++) {
+  dogs.push(prompt(`${i+1}. dog:`));
+}
+
+dogs.sort();
+dogs.reverse();
+
+let uoList = `<ul>`
+
+for (let i = 0; i < dogs.length; i++) {
+  uoList += `<li> ${dogs[i]} </li>`
+}
+
+uoList += `</ol>`
+
+document.querySelector('#target').innerHTML += uoList
+
