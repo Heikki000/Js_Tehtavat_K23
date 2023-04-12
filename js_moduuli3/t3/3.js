428ed4d588cb02490bfe9f9f7ff5074331c2afdf
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-04-12 15:06:01 +0300

    toimii 12.4.

---
+++ js_moduuli3/t3/3.js
@@ -0,0 +1,9 @@
+'use strict';
+const names = ['John', 'Paul', 'Jones'];
+
+const targetElement = document.getElementById('target')
+
+for (let i= 0; i < names.length; i++) {
+  targetElement.innerHTML += `<li>${names[i]}</li>`
+  console.log(i)
+}
