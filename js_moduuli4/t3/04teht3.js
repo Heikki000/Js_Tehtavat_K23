f0ec2fa3ba0b41c6eb95ea4e45e381a120778595
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-04-23 22:54:46 +0300

    valmis 23.4.

--- js_moduuli4/t3/04teht3.js
+++ js_moduuli4/t3/04teht3.js
@@ -3,6 +3,9 @@
 // nettiin lähetettävän hakulauseen alkuosa (vakio)
 const hakuAlku = "https://api.tvmaze.com/search/shows?q="
 
+// etsitään html-sivulta div-elementti
+const tulosAlue = document.querySelector('#results')
+
 // etsitään html-sivun form-elementti
 const tvForm = document.querySelector('#tv');
 
@@ -24,9 +27,53 @@ tvForm.addEventListener('submit', async function (evt) {
       hakuLause
     ); // starting data download, fetch returns a promise which contains an object of type 'response'
     const jsonData = await response.json(); // retrieving the data retrieved from the response object using the json() function
-    console.log(jsonData); // log the result to the console
+
+    // Kutsutaan funktiota, joka hoitaa tietojen esittämisen web-sivulle.
+    // Funktion kutsu on nyt aivan tavallinen funktion kutsu,
+    // edellisen komennon 'await' varmistaa että alla oleva kutsu suoritetaan
+    // vasta kun 'jsonData' on varmasti saatavissa.
+    kasitteleData(jsonData)
 
   } catch (error) {
     console.log(error.message);
   }
 });
+function kasitteleData(jsonData) {
+    // tyhjennetään aluksi tulosalue
+    tulosAlue.innerHTML = ''
+
+    // haetaan tv-sarjasta tietoja ja lisätään ne html-sivulle
+    for (let tvSarja of jsonData) {
+
+      let h2Elem = document.createElement('h2')
+      h2Elem.innerText = tvSarja.show.name
+
+      //linkki
+      let linkElem = document.createElement('a')
+      linkElem.href = tvSarja.show.url
+      linkElem.target = '_blank'
+      linkElem.innerHTML = tvSarja.show.url
+
+      //lisätään kuva
+      let imgElem = document.createElement('img')
+      //varaudutaan medium kuvan puuttumiseen ?-operaattorilla
+      // jos image.medium löytyy, niin otetaan sen arvo
+      // jos medium kuvaa ei löydy, niin arvoksi tulee 'undefined'.
+      imgElem.src = tvSarja.show.image?.medium
+      imgElem.alt = tvSarja.show.name
+
+      let divElem = document.createElement('div')
+      divElem.innerHTML = tvSarja.show.summary
+
+      // luodaan article-elementti, jonka sisään sarjan tiedot tulevat
+      let articleElem = document.createElement('article')
+      articleElem.appendChild(h2Elem)
+      articleElem.appendChild(linkElem)
+      articleElem.appendChild(imgElem)
+      articleElem.appendChild(divElem)
+
+      // lisätään luotu article html-sivulle
+      tulosAlue.append(articleElem)
+    }
+
+  }
