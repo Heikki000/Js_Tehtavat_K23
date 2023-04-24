6060295a1260cda92e20440b3d4033e2818d7484
Heikki000 <heikki.savolainen2@metropolia.fi>
2023-04-24 20:56:44 +0300

    valmis 24.4.

--- js_moduuli4/t1/04teht1.js
+++ js_moduuli4/t1/04teht1.js
@@ -1,11 +1,14 @@
-document.querySelector('#form').addEventListener('submit', async function(evt) {
-    evt.preventDefault();
-        try {
-        const response = await fetch (`https://api.tvmaze.com/search/shows?q=${query.value}`);
-        const jsonData = await response.json();
-        console.log(jsonData);
-    }
-    catch (error){
 
-    }
+document.querySelector('form').addEventListener('submit', async function(evt) {
+  evt.preventDefault();
+  const query = document.querySelector('#query').value;
+  console.log(query)
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+    const jsonData = await response.json();
+    console.log('Response:', response);
+    console.log('JSON Data:', jsonData);
+  } catch (error) {
+    console.log('Error:', error);
+  }
 });
